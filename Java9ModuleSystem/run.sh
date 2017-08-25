77956f155380f1a1fbc0dcf52fb2887a0b4763c4
Hüseyin Akdoğan <hakdogan75@gmail.com>
2017-08-25 11:47:39 +0300

    updated travis.yml

--- Java9ModuleSystem/run.sh
+++ Java9ModuleSystem/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+#You must customize the path of JDK according to your own system.
 export JDK_HOME='/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home'
+#You must customize the path of local maven repository according to your own system.
 $JDK_HOME/bin/java --module-path database/target/database-1.0-SNAPSHOT.jar:info/target/info-1.0-SNAPSHOT.jar:user/target/user-1.0-SNAPSHOT.jar:/Users/hakdogan/.m2/repository/org/apache/logging/log4j/log4j-core/2.8.2/log4j-core-2.8.2.jar:/Users/hakdogan/.m2/repository/org/apache/logging/log4j/log4j-api/2.8.2/log4j-api-2.8.2.jar:/Users/hakdogan/.m2/repository/com/h2database/h2/1.4.196/h2-1.4.196.jar -m info/com.kodcu.info.InfoUser
