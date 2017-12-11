fb2dcd13000b78da621bc2b0a2c5e2cba53d8089
Hüseyin Akdoğan <hakdogan75@gmail.com>
2017-12-11 20:44:39 +0300

    pom files reorganized

--- Java9ModuleSystem/run.sh
+++ Java9ModuleSystem/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export JDK_HOME='/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home' #You must customize the path of JDK according to your own system.
-$JDK_HOME/bin/java --module-path libs -m info/com.kodcu.info.InfoUser
+$JDK_HOME/bin/java --module-path libs:database/libs -m info/com.kodcu.info.InfoUser
